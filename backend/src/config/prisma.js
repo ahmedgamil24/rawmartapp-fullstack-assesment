d0a39e8a880d11b2bf5921e3e9638393af5e2ef7
ahmedgamil24 <agemail5050@gmail.com>
2025-12-29 12:00:05 +0200

    Fix Prisma Config

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,6 +1,17 @@
-import pkg from '@prisma/client';
-const { PrismaClient } = pkg;
+// import pkg from "../generated/prisma/";
+// const { PrismaClient } = pkg;
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-export default prisma;
+// export default prisma;
+
+import { PrismaPg } from "@prisma/adapter-pg";
+import ENV from "./env.js";
+import { PrismaClient } from "@prisma/client";
+
+const connectionString = `${ENV.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export default prisma
